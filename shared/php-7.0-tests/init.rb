852f7759df3648858ff6310ac4cd1c1165a4d245
John noakes <brian.wilkinson@fasthosts.com>
2018-07-17 10:27:54 +0100

    More tests that look for 1.2.3.4

--- shared/php-7.0-tests/init.rb
+++ shared/php-7.0-tests/init.rb
@@ -23,7 +23,7 @@ RSpec.shared_examples "php-7.0-tests" do
   Specinfra::Runner.send_file( phpinfo, "/var/www/html/")
 
   describe command("curl -sS -H \"X-Forwarded-For: 1.2.3.4\" -H \"X-Forwarded-Port: 99\" http://127.0.0.1:#{LISTEN_PORT}/phpinfo.php | grep \"REMOTE_ADDR\"") do
-    its(:stdout) { should contain "1.2.3.4" }
+    its(:stdout) { should contain "1.2.3.???" }
     its(:stderr) { should eq "" }
   end
 
